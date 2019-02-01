9552d03b101dfebb01419ce15dd31309827e170a
shreyjain203 <shreyjain203@gmail.com>
2019-02-01 18:43:37 +0530

    a program that convert speech to text and he text to speech in chinese ascent

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,118 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final int REQ_CODE_SPEECH_INPUT=1000;
+    TextView textView;
+    Button button;
+    Button button2;
+    TextToSpeech speech;
+    String speaker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+
+        button = (Button) findViewById(R.id.button);
+
+        speech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status!= TextToSpeech.ERROR){
+                    speech.setLanguage(Locale.CHINESE);
+
+                }
+            }
+        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
+
+
+        button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              spoke();
+            }
+        });
+
+
+
     }
-}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        switch(requestCode){
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    //get array from voice intent
+                    ArrayList <String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    //set to text view
+                    textView.setText(result.get(0));
+                    speaker = result.get(0);
+                }
+            }
+        }
+
+
+
+
+
+    }
+
+    private void promptSpeechInput(){
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"hiiiiiiii");
+
+        try{ startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+
+    }catch (Exception e) {
+
+        }
+
+        }
+
+
+
+    private void spoke() {
+
+        speech.speak(speaker, TextToSpeech.QUEUE_FLUSH, null);
+
+
+    }
+    }
+
+
+
